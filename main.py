b4673ebcc517e96231f52805496666ddba3f4cf6
BokuHina <158168153+BokuHina@users.noreply.github.com>
2025-12-22 10:41:06 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -13,11 +13,20 @@ trip1_data = AddTrip("Kiyv", "Lviv", "10:00", 15, 500.0, "Петро")
 trip2_data = AddTrip("Odessa", "Harkiv", "12:00", 30, 800.0, "Іван")
 trip3_data = AddTrip("Odessa", "Kiyv", "14:00", 20, 600.0, "Сидор")
 trip4_data = AddTrip("Lviv", "Kiyv", "16:00", 10, 550.0, "Марія")
+trip5_data = AddTrip("Harkiv", "Odessa", "18:00", 25, 750.0, "Олена")
+trip6_data = AddTrip("Lviv", "Odessa", "20:00", 18, 700.0, "Андрій")
+trip7_data = AddTrip("Kiyv", "Harkiv", "22:00", 12, 650.0, "Віктор")
+trip8_data = AddTrip("Harkiv", "Lviv", "09:00", 22, 720.0, "Світлана")
 
 trip_servis.create_trip(trip1_data)
 trip_servis.create_trip(trip2_data)
 trip_servis.create_trip(trip3_data)
 trip_servis.create_trip(trip4_data)
+trip_servis.create_trip(trip5_data)
+trip_servis.create_trip(trip6_data)
+trip_servis.create_trip(trip7_data)
+trip_servis.create_trip(trip8_data)
+
 @app.get('/')
 
 def read_root():
