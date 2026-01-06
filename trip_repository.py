fa206589dd72f559b59c85fce4c78976da216986
BokuHina <158168153+BokuHina@users.noreply.github.com>
2026-01-06 16:30:21 +0200

    Add files via upload

--- trip_repository.py
+++ trip_repository.py
@@ -1,29 +1,7 @@
-from abc import ABC, abstractmethod
+
 from typing import List, Optional, Dict
 from trip_model import AddTrip, Trip
-# абстрактний клас-сховище
-
-class TripRepositoryAbc(ABC):
-
-    @abstractmethod
-    def add(self, trip: AddTrip) -> None:
-        pass
-
-    @abstractmethod
-    def get_by_id(self, trip_id: int) -> Optional[Trip]:
-        pass
-
-    @abstractmethod
-    def get_all(self) -> List[Trip]:
-        pass
-
-    @abstractmethod
-    def update(self, trip: Trip) -> None:
-        pass
-
-    @abstractmethod
-    def get_next_id(self) -> int:
-        pass
+from sqlite_repository import TripRepositoryAbc
 
 
 class TripRepository(TripRepositoryAbc):
@@ -50,7 +28,7 @@ class TripRepository(TripRepositoryAbc):
     def get_by_id(self, trip_id: int) -> Optional[Trip]:
         return self._trips.get(trip_id)
 
-    def get_all(self) -> List[Trip]:
+    def list(self) -> List[Trip]:
         return list(self._trips.values())
 
     def update(self, trip: Trip) -> None:
